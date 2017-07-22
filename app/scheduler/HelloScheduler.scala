0fb2604c551d2b67ca1b2a502b94505c090d4a03
fayimora <fayi@fayimora.com>
2017-07-22 19:13:46 +0100

    Move Actor into Scheduler for simplicity

--- app/scheduler/HelloScheduler.scala
+++ app/scheduler/HelloScheduler.scala
@@ -1,12 +1,21 @@
 package scheduler
 
-import javax.inject.{ Inject, Named }
+import javax.inject.{ Inject, Named, Singleton }
 
-import akka.actor.{ ActorRef, ActorSystem, Cancellable }
+import akka.actor.{ Actor, ActorRef, ActorSystem }
 import play.api.Configuration
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
+
+
+@Singleton
+class HelloJobActor @Inject()()(implicit ec: ExecutionContext) extends Actor {
+  override def receive: Receive = {
+    case s: String =>
+      play.api.Logger.info(s"Hello $s!")
+  }
+}
 
 class HelloScheduler @Inject()(val system: ActorSystem,
                                @Named("hello-actor") val schedulerActor: ActorRef,
